fb9e017504b32bd853083632b0f3a9304cb22182
df <dfd@gad.fasd>
2022-04-20 15:48:52 +0900

    Changes

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
+
 from flask import Flask, render_template, request, blueprints
 from routes.food_reg_route import bp as food_reg_bp
+from random import randrange
 
-app = Flask(__name__)
-app.register_blueprint(food_reg_bp)
-
-@app.route('/')
-def hello_world():  # put application's code here
-	return 'Hello World!'
+from flask import Flask, render_template
+from pyecharts.globals import ThemeType
+from pyecharts import options as opts
+from pyecharts.charts import Bar,Pie
 
 
-if __name__ == '__main__':
-	app.run()
+app = Flask(__name__, static_folder="templates")
+
+
+def pie_base() -> Pie:
+    a = [123,456,789]
+    v = [[i] for i in a]
+    k = ['김기록','김승헌','최용훈']
+    d=[[j] for j in k]
+    c = (
+        Pie(init_opts=opts.InitOpts(theme=ThemeType.WESTEROS)
+            ).add("", [list(z) for z in zip(k, v)],rosetype="radius", radius=["30%", "60%"]
+                                                                   ).set_series_opts(label_opts=opts.LabelOpts(is_show=True, position='top')
+                                                                                     ).set_global_opts(title_opts=opts.TitleOpts(title="음식 영양 정보"),
+                     legend_opts=opts.LegendOpts(type_='scroll', pos_bottom="60%", pos_right="-3%", orient="vertical",legend_icon='pin'))
+    )
+    return c
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/pieChart")
+def get_pie_chart():
+    c = pie_base()
+    return c.dump_options_with_quotes()
+
+
+if __name__ == "__main__":
+    app.run()
