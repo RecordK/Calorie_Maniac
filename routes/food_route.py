35b552015f88d0edaf7774d441ef666a236f72a3
ChoiRang <jkchldyd@gmail.com>
2022-05-07 14:35:49 +0900

    modify: food_route

--- routes/food_route.py
+++ routes/food_route.py
@@ -1,9 +1,11 @@
 from _datetime import datetime
 from flask import Blueprint, Flask, render_template, request, session
 from python_files.food.food_info_service import FoodInfoService
+from python_files.food.food_history_service import FoodHistoryService
 
 bp = Blueprint('food', __name__, url_prefix='/main/food')
 food_info_service = FoodInfoService()
+food_history_service = FoodHistoryService()
 
 @bp.get('/')
 def food_page():
@@ -14,16 +16,16 @@ def food_page():
 def food_search():
 	food_name = request.form['food_name']
 	food_name = '%' + food_name + '%'
-	food_list = food_info_service.retrieve_name(food_name)
+	food_list = food_info_service.retrieve_by_name(food_name)
 	return render_template('loader/food_list.html', food_list=food_list)
 
 
 @bp.get('/register')
 def get_index():
-	index = request.args.get('fid')
-	today = datetime.today().strftime("%Y-%m-%d %H:%M")
-	print(today)
-	print(index)
+	food_info_index = request.args.get('fid')
+	food = food_info_service.retrieve_by_index(food_info_index)
+	food_image = None
+	food_history_service.insert_data(food.food_index, food.food_name, food_image)
 	return render_template('index.html')
 
 
