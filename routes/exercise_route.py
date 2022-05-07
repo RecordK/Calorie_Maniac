6784c1da9a5fb8f210057af45d49d8079a2c1b65
ChoiRang <jkchldyd@gmail.com>
2022-05-07 11:42:24 +0900

    re: exercise-route

--- routes/exercise_route.py
+++ routes/exercise_route.py
@@ -8,11 +8,6 @@ bp = Blueprint('health', __name__, url_prefix='/main/health')
 @bp.get('/')
 def main():
 	exercise_list = [
-		'push_up.png', 'squat.png', 'crunch.png',
-		'lying_reg_raise.png', 'dips.png', 'bicycle_crunch.png',
-		'cross_lunge.png', 'pull_up.png', 'side_lateral_raise.png'
-	]
-	exercise_list = [
 		['push_up.png', '푸쉬업'],
 		['squat.png', '스쿼트'],
 		['crunch.png', '크런치'],
@@ -23,10 +18,11 @@ def main():
 		['pull_up.png', '폴업'],
 		['side_lateral_raise.png', '사이드 레터럴 레이즈']
 	]
-	for i in exercise_list:
-		print(i)
-		print(i[0])
-		print(i[0][0])
-	print(exercise_list)
-	exercise_name = ['푸쉬업', '스쿼트', '크런치']
 	return render_template('exercise_page.html', exercise_list=exercise_list)
+
+
+@bp.get('/realtime')
+def move_page():
+	index = request.args.get('index')
+	print(index)
+	return index
